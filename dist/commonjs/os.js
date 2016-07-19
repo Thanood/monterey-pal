1730aca4e2493d1e4efdc1d6e3dec474b3fbc9a1
Jeroen <Jeroenvinke17@gmail.com>
2016-07-19 12:08:03 +0200

    feat(OS): expose versions of node/npm/chrome/electron

--- dist/commonjs/os.js
+++ dist/commonjs/os.js
@@ -6,5 +6,17 @@ Object.defineProperty(exports, "__esModule", {
 var OS = exports.OS = {
   getPlatform: function getPlatform() {
     throw new Error('Not implemented');
+  },
+  getNodeVersion: function getNodeVersion() {
+    throw new Error('Not implemented');
+  },
+  getNPMVersion: function getNPMVersion() {
+    throw new Error('Not implemented');
+  },
+  getChromeVersion: function getChromeVersion() {
+    throw new Error('Not implemented');
+  },
+  getElectronVersion: function getElectronVersion() {
+    throw new Error('Not implemented');
   }
 };
--- src/os.js
+++ src/os.js
@@ -1,5 +1,17 @@
 export const OS = {
   getPlatform() {
     throw new Error('Not implemented');
+  },
+  getNodeVersion() {
+    throw new Error('Not implemented');
+  },
+  getNPMVersion() {
+    throw new Error('Not implemented');
+  },
+  getChromeVersion() {
+    throw new Error('Not implemented');
+  },
+  getElectronVersion() {
+    throw new Error('Not implemented');
   }
 }
